b010b34f8d9264684772b15a49b2da478bbaec45
Harini <harinivs@thoughtworks.com>
2019-12-28 12:12:20 +0530

    Harini: Introduce a CarList component to list the car details

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {View} from 'react-native';
 import Header from './Header';
+import CarList from './CarList';
 
 const App = () => {
-  return <Header title={'Cars'} />;
+  return (
+    <View>
+      <Header title={'Cars'} />
+      <CarList />
+    </View>
+  );
 };
 
 export default App;
---
+++ src/components/CarList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const CarList = () => {
+  return (
+    <View>
+      <Text>Car List</Text>
+    </View>
+  );
+};
+
+export default CarList;
